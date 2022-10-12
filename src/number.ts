ced74ca9c98571db75e3dcca36fef32d1393dcb3
Limonadovy-joe <chossecko@gmail.com>
2022-10-12 17:42:33 +0200

    feat(number): add magma operations

--- src/number.ts
+++ src/number.ts
@@ -1,8 +1,31 @@
 import { Eq } from './Eq';
+import { Magma } from './Magma';
+import { Semigroup } from './Semigroup';
 
 /**
  * @category instancies
  */
 export const eq: Eq<number> = {
   equals: (x, y) => x === y
+};
+
+/**
+ * @category instancies
+ */
+export const MagmaSub: Magma<number> = {
+  concate: (fst, snd) => fst - snd
+};
+
+/**
+ * @category instancies
+ */
+export const SemigroupSum: Semigroup<number> = {
+  concate: (fst, snd) => fst + snd
+};
+
+/**
+ * @category instancies
+ */
+export const SemigroupProduct: Semigroup<number> = {
+  concate: (fst, snd) => fst * snd
 };

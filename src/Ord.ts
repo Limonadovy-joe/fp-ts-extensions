51980c2c0ee7448287b40accefcd403e5bdff38d
Limonadovy-joe <chossecko@gmail.com>
2022-10-19 10:24:29 +0200

    feat(ord): add other utils functions

--- src/Ord.ts
+++ src/Ord.ts
@@ -31,3 +31,91 @@ export const min =
   <A>({ compare, equals }: Ord<A>) =>
   (x: A, y: A) =>
     equals(x, y) || compare(x, y) <= 0 ? x : y;
+
+/**
+ * @category utils
+ */
+export const max =
+  <A>({ compare, equals }: Ord<A>) =>
+  (x: A, y: A) =>
+    equals(x, y) || compare(x, y) === -1 ? y : x;
+
+/**
+ * @category utils
+ */
+export const tuple = <A extends ReadonlyArray<unknown>>(...ords: { [K in keyof A]: Ord<A[K]> }): Ord<Readonly<A>> => ({
+  compare: (tupleA, tupleB) =>
+    ords.reduce<Ordering>((acc, ord, index) => ord.compare(tupleA[index], tupleB[index]) || acc, 0),
+  equals: (tupleA, tupleB) => ords.every(({ compare }, index) => compare(tupleA[index], tupleB[index]) === 0)
+});
+
+export const reverse = <A>(ord: Ord<A>): Ord<A> => ({
+  compare: (x, y) => [ord.compare(x, y)].map((ordering) => (ordering !== 0 ? -ordering : 0))[0] as Ordering,
+  equals: equalsDefault(ord.compare)
+});
+
+export const clamp =
+  <A>(ord: Ord<A>) =>
+  (x: A, y: A) =>
+  (val: A): A => {
+    const minimum = min(ord);
+    const maximum = max(ord);
+
+    return val <= x ? maximum(x, val) : minimum(y, val);
+  };
+
+/**
+ * Lower then
+ * Tests whether x is lower then y
+ * x < y
+ */
+export const lt =
+  <A>({ compare }: Ord<A>) =>
+  (x: A, y: A): boolean =>
+    compare(x, y) === -1;
+
+/**
+ * Greater then
+ * Tests whether x is greater then y
+ * x > y
+ */
+export const gt =
+  <A>({ compare }: Ord<A>) =>
+  (x: A, y: A): boolean =>
+    compare(x, y) === 1;
+
+/**
+ * Lower then equal
+ * Tests whether x is lower then  equal y
+ * x <= y
+ */
+export const leq =
+  <A>(ord: Ord<A>) =>
+  (x: A, y: A): boolean =>
+    ord.compare(x, y) === 0 || lt(ord)(x, y);
+
+/**
+ * Greater then equal
+ * Tests whether x is greater then equal y
+ * x <= y
+ */
+export const geq =
+  <A>(ord: Ord<A>) =>
+  (x: A, y: A): boolean =>
+    ord.compare(x, y) === 0 || gt(ord)(x, y);
+
+/**
+ * Tests whether val is between min and max inclusive
+ */
+export const between =
+  <A>(ord: Ord<A>) =>
+  (x: A, y: A) =>
+  (val: A): boolean =>
+    clamp(ord)(x, y)(val) === val;
+
+export const contramap =
+  <A, B>(map: (a: B) => A) =>
+  ({ equals, compare }: Ord<A>): Ord<B> => ({
+    equals: (x, y) => equals(map(x), map(y)),
+    compare: (x, y) => compare(map(x), map(y))
+  });

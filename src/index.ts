6a3074734bcdfd744e2c9dba05120b40d4ef996c
Limonadovy-joe <chossecko@gmail.com>
2022-10-20 11:04:35 +0200

    feat(packaging): change index.ts to support re-exports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import * as path from 'path';
-import {ReaderIOEither} from './ReaderIOEither'
-import {right} from 'fp-ts/lib/Either';
+import * as eq from './Eq';
+import * as ord from './Ord';
+import * as fun from './function';
+import * as string from './string';
+import * as number from './number';
+import * as boolean from './boolean';
 
-console.log(process.env);
-console.log( '__dirname join', path.join(__dirname, '..'));
-console.log( '__dirname normalize', path.normalize(path.join(__dirname, '..')));
-
-export const decodeData: (data: string) => ReaderIOEither<{decoder: boolean}, Error, string> = (data) => ({decoder}) => {
-console.log('decodeData', 'data', data, 'decoder', decoder);
-return () => right<Error, string>(data);
-} 
-
-decodeData('{data: [1,2,3,5]}');
+export { eq, ord, fun, string, number, boolean };
